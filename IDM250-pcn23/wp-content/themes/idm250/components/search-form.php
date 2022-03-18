769a49eedda0a39665b6932f34e8e8f42390a523
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-03-18 02:28:03 -0400

    get in line search thingy

--- IDM250-pcn23/wp-content/themes/idm250/components/search-form.php
+++ IDM250-pcn23/wp-content/themes/idm250/components/search-form.php
@@ -1,4 +1,4 @@
-<form action="<?php echo home_url()?>" method="GET">
+<form action="<?php echo home_url()?>" method="GET" class="searchthing">
     <input placeholder="Search here!" type="text" name="s" value="">
     <input type="hidden" name="post_type[]" value="idm-projects">
     <button>Go</button>
