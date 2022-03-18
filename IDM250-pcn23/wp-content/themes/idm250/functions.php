f3fdcc0afb2c7de696be2330f3e7f87dcd344f97
Parker Nix <71665527+ParkerNix@users.noreply.github.com>
2022-03-18 00:23:03 -0400

    archive and about

--- IDM250-pcn23/wp-content/themes/idm250/functions.php
+++ IDM250-pcn23/wp-content/themes/idm250/functions.php
@@ -111,4 +111,37 @@ function idm_register_sidebars() {
 
 add_action('widget_init', 'idm_register_sidebars');
 
+function idm_register_taxonomies()
+{
+    $labels = [
+        'name' => _x('Categories', 'taxonomy general name'),
+        'singular_name' => _x('Category', 'taxonomy singular name'),
+        'search_items' => __('Search Categories'),
+        'all_items' => __('All Categories'),
+        'parent_item' => __('Parent Category'),
+        'parent_item_colon' => __('Parent Category:'),
+        'edit_item' => __('Edit Category'),
+        'update_item' => __('Update Category'),
+        'add_new_item' => __('Add New Category'),
+        'new_item_name' => __('New Category Name'),
+        'menu_name' => __('Categories'),
+    ];
+
+    register_taxonomy(
+        'idm-project-categories',
+        ['idm-projects'],
+        [
+            'hierarchical' => true,
+            'labels' => $labels,
+            'show_ui' => true,
+            'show_in_rest' => true,
+            'show_admin_column' => true,
+            'query_var' => true,
+            'rewrite' => ['slug' => 'works'],
+        ]
+    );
+}
+
+add_action('init', 'idm_register_taxonomies', 0);
+
 ?>
--- IDM250-pcn23/wp-content/themes/idm250/templates/template-about.php
+++ IDM250-pcn23/wp-content/themes/idm250/templates/template-about.php
@@ -3,18 +3,14 @@
 ?>
 <?php get_header(); ?>
 <div class="fill">
-    <div class="back">
         <?php while (have_posts()) : the_post(); ?>
         <div class="">
             <h1 class="title"><?php the_title(); ?></h1>
-
-            <?php the_post_thumbnail(); ?>
         
-            <div class="">
+            <div class="aboutstuff">
                 <?php the_content(); ?>
             </div>
         </div>
         <?php endwhile; ?>
-    </div>
 </div>
 <?php get_footer(); ?>
